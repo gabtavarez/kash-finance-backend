d1bedf73a31a019549d0db8d95a47933044c2b62
gabtavarez <gabrieltavarescs03@gmail.com>
2026-09-13 20:39:30 -0300

    feat: criar entidade Transacao

---
+++ src/main/java/tavarez/kash_finance/model/Transacao.java
@@ -0,0 +1,83 @@
+package tavarez.kash_finance.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import tavarez.kash_finance.util.Relogios;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "transacoes")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+public class Transacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private Usuario usuario;
+
+    @ManyToOne
+    @JoinColumn(name = "serie_id")
+    private SerieTransacao serie;
+
+    @ManyToOne
+    @JoinColumn(name = "conta_id", nullable = false)
+    private Conta conta;
+
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
+    private Categoria categoria;
+
+    @Column(nullable = false)
+    private String descricao;
+
+    @Column(nullable = false, precision = 15, scale = 2)
+    private BigDecimal valor;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TipoTransacao tipo;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private StatusTransacao status;
+
+    @Column(name = "data_transacao", nullable = false)
+    private LocalDate dataTransacao;
+
+    @Column(name = "parcela_atual")
+    private Integer parcelaAtual;
+
+    @ManyToOne
+    @JoinColumn(name = "transacao_relacionada_id")
+    private Transacao transacaoRelacionada;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = Relogios.agora();
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = Relogios.agora();
+    }
+}

9fd248298970cc4da169c1a72c85c2f021ba2871
gabtavarez <gabrieltavarescs03@gmail.com>
2026-09-14 23:50:37 -0300

    feat: adicionar busca de categorias raiz

--- src/main/java/tavarez/kash_finance/repository/CategoriaRepository.java
+++ src/main/java/tavarez/kash_finance/repository/CategoriaRepository.java
@@ -3,5 +3,9 @@ package tavarez.kash_finance.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tavarez.kash_finance.model.Categoria;
 
+import java.util.List;
+
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+    List<Categoria> findByCategoriaPaiIsNull();
 }

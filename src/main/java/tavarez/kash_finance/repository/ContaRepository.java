9038e0e3a7dad4061fa65822f2a02693fa0d7049
gabtavarez <gabrieltavarescs03@gmail.com>
2026-09-16 23:24:06 -0300

    feat: adicionar busca de contas por usuario

--- src/main/java/tavarez/kash_finance/repository/ContaRepository.java
+++ src/main/java/tavarez/kash_finance/repository/ContaRepository.java
@@ -3,5 +3,9 @@ package tavarez.kash_finance.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tavarez.kash_finance.model.Conta;
 
+import java.util.List;
+
 public interface ContaRepository extends JpaRepository<Conta, Long> {
+
+    List<Conta> findByUsuarioId(Long usuarioId);
 }

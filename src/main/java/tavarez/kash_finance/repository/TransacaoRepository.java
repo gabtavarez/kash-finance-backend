05636bcacf5d02b00ca1d9967c9dc7024f28bc05
gabtavarez <gabrieltavarescs03@gmail.com>
2026-09-20 22:23:51 -0300

    feat: adicionar busca de transacoes por conta

--- src/main/java/tavarez/kash_finance/repository/TransacaoRepository.java
+++ src/main/java/tavarez/kash_finance/repository/TransacaoRepository.java
@@ -3,5 +3,9 @@ package tavarez.kash_finance.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tavarez.kash_finance.model.Transacao;
 
+import java.util.List;
+
 public interface TransacaoRepository extends JpaRepository<Transacao, Long> {
+
+    List<Transacao> findByContaId(Long contaId);g
 }
